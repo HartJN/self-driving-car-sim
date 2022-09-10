5b274c206d652c8aec0c2eb0f668d20feb36132d
HartJN <jameshart04@gmail.com>
2022-09-11 09:51:34 +1200

    feat: draw sensor rays

--- car.js
+++ car.js
@@ -12,6 +12,8 @@ class Car {
     this.friction = 0.05
     this.angle = 0
 
+    this.sensor = new Sensor(this)
+
     this.controls = new Controls()
   }
 
@@ -19,6 +21,7 @@ class Car {
 
   update() {
     this.#move()
+    this.sensor.update()
   }
   #move() {
     if (this.controls.forward) {
@@ -68,5 +71,7 @@ class Car {
     ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height)
     ctx.fill()
     ctx.restore()
+
+    this.sensor.draw(ctx)
   }
 }
