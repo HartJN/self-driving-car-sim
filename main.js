a3ea6f3523131e1f0ebbc2fadc742cd7f7999440
HartJN <jameshart04@gmail.com>
2022-09-11 12:18:21 +1200

    refactor: set traffic max speed value to 2

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const ctx = canvas.getContext('2d')
 const road = new Road(canvas.width / 2, canvas.width * 0.9)
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, 'KEYS')
 // add array of cars(traffic)
-const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY')]
+const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY', 2)]
 
 //animate car
 
