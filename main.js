986952c763a8ea4b2558ed088eb111c8598c1668
HartJN <jameshart04@gmail.com>
2022-09-11 12:11:08 +1200

    feat: populate road with traffic

--- main.js
+++ main.js
@@ -5,12 +5,17 @@ canvas.width = 200
 const ctx = canvas.getContext('2d')
 const road = new Road(canvas.width / 2, canvas.width * 0.9)
 const car = new Car(road.getLaneCenter(1), 100, 30, 50)
+// add array of cars(traffic)
+const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50)]
 
 //animate car
 
 animate()
 
 function animate() {
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].update(road.borders)
+  }
   car.update(road.borders)
 
   canvas.height = window.innerHeight
@@ -21,6 +26,10 @@ function animate() {
   ctx.translate(0, -car.y + canvas.height * 0.7)
   road.draw(ctx)
 
+  // draw traffic cars
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].draw(ctx)
+  }
   car.draw(ctx)
 
   ctx.restore()
