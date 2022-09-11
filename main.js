f83f37e3de1df606b57b3c9573a3518faa97dc74
HartJN <jameshart04@gmail.com>
2022-09-11 16:11:35 +1200

    feat: store 'best car' in local storage

--- main.js
+++ main.js
@@ -11,6 +11,10 @@ const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9)
 
 const N = 100
 const cars = generateCars(N)
+let bestCar = cars[0]
+if (localStorage.getItem('bestBrain')) {
+  bestCar.brain = JSON.parse(localStorage.getItem('bestBrain'))
+}
 
 // add array of cars(traffic)
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY', 2)]
@@ -18,6 +22,15 @@ const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY', 2)]
 //animate car
 
 animate()
+
+// save car locally
+
+function save() {
+  localStorage.setItem('bestBrain', JSON.stringify(bestCar.brain))
+}
+function discard() {
+  localStorage.removeItem('bestBrain')
+}
 
 // generate cars
 
@@ -37,7 +50,7 @@ function animate(time) {
     cars[i].update(road.borders, traffic)
   }
 
-  const bestCar = cars.find(c => c.y == Math.min(...cars.map(c => c.y)))
+  bestCar = cars.find(c => c.y == Math.min(...cars.map(c => c.y)))
 
   carCanvas.height = window.innerHeight
   networkCanvas.height = window.innerHeight
