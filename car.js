bb3d6693a1b8ac846e61e787680b220fb31a4f3f
HartJN <jameshart04@gmail.com>
2022-09-11 14:41:39 +1200

    feature: add neural networks
    Level class provides weights and biases for neural network simulation.

--- car.js
+++ car.js
@@ -17,6 +17,7 @@ class Car {
     if (controlType != 'DUMMY') {
       // pass car to this
       this.sensor = new Sensor(this)
+      this.brain = new NeuralNetwork([this.sensor.rayCount, 6, 4])
     }
 
     this.controls = new Controls(controlType)
@@ -35,6 +36,11 @@ class Car {
     }
     if (this.sensor) {
       this.sensor.update(roadBorders, traffic)
+      // const offsets = this.sensor.readings.map(s =>
+      //   s == null ? 0 : 1 - s.offset
+      // )
+      // const outputs = NeuralNetwork.feetForward(offsets, this.brain)
+      // console.log(outputs)
     }
   }
 
---
+++ network.js
@@ -0,0 +1,70 @@
+// neuronCounts = array of neurons in each layer
+class NeuralNetwork {
+  constructor(neuronCounts) {
+    this.levels = []
+    for (let i = 0; i < neuronCounts.length - 1; i++) {
+      this.levels.push(new Level(neuronCounts[i], neuronCounts[i + 1]))
+    }
+  }
+
+  static feetForward(givenInputs, network) {
+    let outputs = Level.feetForward(givenInputs, network.levels[0])
+    for (let i = 1; i < network.levels.length; i++) {
+      outputs = Level.feetForward(outputs, network.levels[i])
+    }
+    return outputs
+  }
+}
+
+// Creates weights
+class Level {
+  constructor(inputCount, outputCount) {
+    this.inputs = new Array(inputCount)
+    this.outputs = new Array(outputCount)
+    // each output neuron has a biases. an output at which it will fire.
+    this.biases = new Array(outputCount)
+
+    // connecting every input to out.
+    this.weights = []
+    // for each input node, there is an outputCount amount of connections.
+    for (let i = 0; i < inputCount; i++) {
+      // create empty array, the size of outputCount
+      this.weights[i] = new Array(outputCount)
+    }
+
+    Level.#randomise(this)
+  }
+
+  static #randomise(level) {
+    for (let i = 0; i < level.inputs; i++) {
+      for (let j = 0; j < level.outputs.length; j++) {
+        level.weights[i][j] = Math.random() * 2 - 1
+      }
+    }
+    for (let i = 0; i < level.biases.length; i++) {
+      level.biases[i] = Math.random() * 2 - 1
+    }
+  }
+  // computer output values using feet forward algorithm
+
+  static feetForward(givenInputs, level) {
+    // goes through all level inputs and sets the to inputs from sensor
+    for (let i = 0; level.inputs.length; i++) {
+      level.inputs[i] = givenInputs[i]
+    }
+    // loop through every output
+    for (let i = 0; i < level.outputs.length; i++) {
+      // calc sum between values of input and weights
+      let sum = 0
+      for (let j = 0; level.inputs.length; j++) {
+        sum += level.inputs[j] * level.weights[j][i]
+      }
+      if (sum > level.biases[i]) {
+        level.outputs[i] = 1
+      } else {
+        level.outputs[i] = 0
+      }
+    }
+    return level.outputs
+  }
+}
